85f0643320a1f77ad25038498bd18d393abd3873
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-14 12:50:00 -0700

    update db config aws

--- config/config.js
+++ config/config.js
@@ -1,13 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const dbConfig = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-};
+const dbConfig =
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL, // Use AWS RDS when in production
+        dialect: "postgres",
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false, // Required for AWS RDS
+          },
+        },
+      }
+    : {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: "postgres",
+      };
 
 export default dbConfig;
