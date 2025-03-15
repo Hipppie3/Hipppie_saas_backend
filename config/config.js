ca28ad555be4e367a87923a6d492581045bf0cb8
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-15 10:56:30 -0700

    fix ssl

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 const dbConfig = {
-  connectionString: process.env.DATABASE_URL || `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  connectionString: process.env.DATABASE_URL || 
+    `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
-      require: process.env.NODE_ENV === 'production', // Only use SSL in production (AWS RDS)
-      rejectUnauthorized: false, // Needed for AWS RDS SSL connection
+      require: true, // Always require SSL for AWS RDS
+      rejectUnauthorized: false, // Allows self-signed certificates
     },
   },
 };
