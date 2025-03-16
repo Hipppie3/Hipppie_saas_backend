0b858cd8ad00adf44deba8ad49baca88e3806b6b
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-15 17:29:08 -0700

    more changes

--- config/database.js
+++ config/database.js
@@ -1,37 +1,15 @@
 import dotenv from 'dotenv';
-import fs from 'fs';
-import path from 'path';
 import { Sequelize } from 'sequelize';
+import dbConfig from './config.js';  // Import configuration from config.js
 
 // Load environment variables
 dotenv.config();
 
+// Choose the correct config based on the environment
+const environment = process.env.NODE_ENV || 'development';
+const config = dbConfig[environment];
 
-const __dirname = new URL('.', import.meta.url).pathname;
-
-const dbConfig = {
-  development: {
-    username: process.env.DB_USERNAME || 'db_manager',
-    password: process.env.DB_PASSWORD || 'your_password',
-    database: process.env.DB_NAME || 'hipppie_saas_db',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
-    logging: false,  // Disable logging in development
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,  // Allow self-signed certificates in production
-        ca: process.env.NODE_ENV === 'production' ? fs.readFileSync(path.join(__dirname, 'rds-ca-2019-root.pem')).toString() : undefined,  // Only include CA certificate in production
-      },
-    },
-  },
-};
-
+// Use DATABASE_URL for production (Heroku, AWS RDS) and fallback to local config
 const sequelize = process.env.DATABASE_URL
   ? new Sequelize(process.env.DATABASE_URL, {   // Use DATABASE_URL for production
       dialect: 'postgres',
@@ -39,19 +17,19 @@ const sequelize = process.env.DATABASE_URL
       dialectOptions: {
         ssl: {
           require: true,
-          rejectUnauthorized: false,  // Fix for self-signed certificate in Heroku production
+          rejectUnauthorized: false,  // Allow self-signed certificates (Heroku, AWS RDS)
         },
       },
     })
   : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USERNAME,
-      process.env.DB_PASSWORD,
+      config.database,
+      config.username,
+      config.password,
       {
-        host: process.env.DB_HOST || '127.0.0.1',  // Default to localhost for local development
-        port: process.env.DB_PORT || 5432,
-        dialect: 'postgres',
-        logging: false,  // Disable logging in development
+        host: config.host,
+        port: config.port,
+        dialect: config.dialect,
+        logging: config.logging,  // Enable logging in development
       }
     );
 
