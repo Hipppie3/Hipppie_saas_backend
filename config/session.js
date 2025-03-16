f2893062b57085461f06ef19cdd7ec959b14138f
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-15 18:10:35 -0700

    again

--- config/session.js
+++ config/session.js
@@ -4,20 +4,21 @@ import { Sequelize } from 'sequelize';
 
 // Session Store Connection for Production (Heroku) and Local (localhost)
 const sessionMiddleware = session({
-  store: new (pgSession(session))({
-    conObject: process.env.NODE_ENV === 'production'
-      ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
-      : { user: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, host: process.env.DB_HOST, database: process.env.DB_NAME, port: process.env.DB_PORT },
-    tableName: 'sessions',
-  }),
-  secret: process.env.SESSION_SECRET || 'supersecretkey',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production', // Set to false for local development
-    httpOnly: true,
-    sameSite: 'None', // For cross-origin cookies in production
-  },
-});
+ store: new (pgSession(session))({
+      conObject: process.env.NODE_ENV === 'production'
+        ? { connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } }
+        : { user: process.env.DB_USERNAME, password: process.env.DB_PASSWORD, host: process.env.DB_HOST, database: process.env.DB_NAME, port: process.env.DB_PORT },
+      tableName: 'sessions',
+    }),
+    secret: process.env.SESSION_SECRET || 'supersecretkey',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // Use false for local development
+      httpOnly: true,
+      sameSite: 'None', // This is necessary for cross-origin cookies
+    },
+  })
+
 
 export default sessionMiddleware;
