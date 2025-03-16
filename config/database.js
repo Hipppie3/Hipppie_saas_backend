df0672f4b41d6c9c24e1f37c8a3af48a33faac2c
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-15 17:19:36 -0700

    changes

--- config/database.js
+++ config/database.js
@@ -39,7 +39,7 @@ const sequelize = process.env.DATABASE_URL
       dialectOptions: {
         ssl: {
           require: true,
-          rejectUnauthorized: true,  // Fix for self-signed certificate in Heroku production
+          rejectUnauthorized: false,  // Fix for self-signed certificate in Heroku production
         },
       },
     })
