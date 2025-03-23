87511523b585b3ad499e050f3754d2fb451b9c04
Hipppie3 <107952593+Hipppie3@users.noreply.github.com>
2025-03-23 10:36:13 -0700

    test

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -358,14 +358,3 @@ const deleteImageFromS3 = async (imageUrl) => {
   }
 };
 
-
-
-export const getPlayerList = async (req, res) => {
-  try {
-    const players = await Player.findAll();
-    res.status(200).json(players)
-  } catch (error) {
-    console.error("Error fetching players", error)
-    res.status(500).json({ message: "Error fetching players"})
-  }
-}
